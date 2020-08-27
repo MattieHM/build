840881e56fd8d5b073c88ca445d28f71c3f9b4c7
MattieHM <mr-polytech@yandex.ru>
2020-08-27 17:26:14 +0300

    Initial commit

---
+++ precache-manifest.23617ce064a4d4c49d9046b88e141b78.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cb33113116379e214f68c427348b6cac",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f67de3d80ac33c8f4e3e",
+    "url": "/static/css/2.847e01e0.chunk.css"
+  },
+  {
+    "revision": "8fc8def845a171ff275a",
+    "url": "/static/css/main.bc1f2740.chunk.css"
+  },
+  {
+    "revision": "f67de3d80ac33c8f4e3e",
+    "url": "/static/js/2.080c07b3.chunk.js"
+  },
+  {
+    "revision": "542c91f071fe828f7759f188e3ae7231",
+    "url": "/static/js/2.080c07b3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8fc8def845a171ff275a",
+    "url": "/static/js/main.505dbb74.chunk.js"
+  },
+  {
+    "revision": "8ed108b970bb0da62a83",
+    "url": "/static/js/runtime-main.f91b59f9.js"
+  }
+]);
